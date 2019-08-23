c23a1b196beb1ef2694d6882ff2e8392203d4ef7
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:37:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  newarray = Array.new
+  array.each do |n|
+    newarray << yield(n)
+  end
+  newarray
+end
+
+def reduce(array, start = NIL)
+  if start
+    final = start
+  else 
+    final = array[0]
+    array.shift
+  end
+  array.each do |n|
+    final = yield(final, n)
+  end
+  final
+end
